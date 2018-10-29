e70586b3fa3581370a5df0ec95a31eac19be7fbe
Pamela Anderson <pamelaanderson519@yahoo.com>
2018-10-29 09:40:47 -0400

    added all the main files

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,39 @@
+const employees = require('../data/employees');
+
+module.exports = function(app) {
+    app.get('./api/employees', function(req,res) {
+        res.json(employees);
+    });
+
+    app.post('./api/employees', function(req, res) {
+        const bestMatch = {
+            name: '',
+            photo: '',
+            employeeDifference: Infinity
+        };
+        const userData = req.body;
+        const userScores = userData.scores;
+
+        let totalDifference;
+
+        for (let i = 0; i < employees.length; i++) {
+            const currentEmployee = employees[i];
+            totalDifference = 0;
+
+            for (let j = 0; j < currentEmployee.scores.length; j++) {
+                const currentEmployeeScore = currentEmployee.scores[j];
+                const currentUserScore = userScores[j];
+
+                totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentEmployeeScore));
+            }
+        }
+        if (totalDifference <= bestMatch.employeeDifference) {
+            bestMatch.name = currentEmployee.name;
+            bestMatch.photo = currentEmployee.photo;
+            bestMatch.employeeDifferent = totalDifference;
+        }
+    })
+    console.log(bestMatch);
+    employees.push(userData);
+    
+};
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ exxtended: true}));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'app/public')));
+
+
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+
+
+app.listen(PORT, function() {
+    console.log(`App listening on PORT: ${PORT}`);
+});
